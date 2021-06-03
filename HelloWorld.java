1588f970c45914821b880545259a37c1a0759192
rouaazayani <83990751+rouaazayani@users.noreply.github.com>
2021-06-03 20:26:35 +0100

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+/** 
+ * La classe HelloWorldApp implémente une application qui imprime simplement "Hello World" sur une sortie standard.
+ */
+ classe  publique HelloWorld {
+  public  static  void  main ( String [] args ) {
+    Système . dehors . println( " Bonjour tout le monde " );
+  }
+}
